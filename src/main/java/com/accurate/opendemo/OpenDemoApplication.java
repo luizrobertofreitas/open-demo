c61228b0284a5c9822b1d116a60da8c67a93c323
Luiz Roberto Freitas <luiz.junior@accurate.com.br>
2019-05-16 15:33:20 -0300

    First version

---
+++ src/main/java/com/accurate/opendemo/OpenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.accurate.opendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDemoApplication.class, args);
+	}
+
+}
